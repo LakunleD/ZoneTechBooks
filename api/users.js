023fa66016384333862cdae464ae08bc6f2b941b
Olakunle Dosunmu <olakunledosunmu@gmail.com>
2018-06-22 20:09:43 +0100

    Set up

---
+++ api/users.js
@@ -0,0 +1,14 @@
+"use strict";
+const express = require('express');
+
+const queries = require('../db/queries');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+
+});
+
+
+
+module.exports = router;
